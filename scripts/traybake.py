07055aba5e1cf8d60856fedb68a7c28beaee060e
STSpencer <sojourner92@gmail.com>
2021-06-09 11:09:13 +0100

    TM brightness results for eta car

--- scripts/traybake.py
+++ scripts/traybake.py
@@ -4,14 +4,19 @@ import numpy as np
 from astropy.table import Table, Column, MaskedColumn
 from matplotlib import cm
 import matplotlib as mpl
+from scipy.ndimage import rotate
+inputtable=Table.read('/home/spencers/sstcam_nsb/results/etacardarkgauss3/fluxes_crota_0.csv')
 
-inputtable=Table.read('fluxes_crota_0.csv')
 print(inputtable)
 posx=inputtable['xcenter'].data
 posy=inputtable['ycenter'].data
 sums=inputtable['aperture_sum'].data
 windowsize_r = 8
 windowsize_c = 8
+mean=np.mean(sums)
+sig=np.std(sums)
+maxval=np.amax(sums)
+minval=np.amin(sums)
 
 def cam_squaremaker(data):
     '''Function to translate CHEC-S integrated images into square arrays for
@@ -37,12 +42,14 @@ def cam_squaremaker(data):
     square = np.flip(square, 0)
     return square
 
-sums=cam_squaremaker(sums)
-
+sums=np.asarray(cam_squaremaker(sums))
+sums=np.rot90(sums,3)
+fig=plt.Figure()
 plt.imshow(sums,cmap=cm.viridis,norm=mpl.colors.LogNorm())
-
-plt.colorbar()
-plt.show()
+plt.title('Engineering Camera Frame\n Mean='+str('%.1f'%mean)+', $\sigma$='+str('%.1f'%sig)+'\n Max='+str('%.1f'%maxval)+', Min='+str('%.1f'%minval))
+plt.colorbar(label='Relative Brightness (nLb/Pixel)')
+plt.tight_layout()
+plt.savefig('nLb_pixel.png')
 
 tms=[]
 
@@ -56,7 +63,18 @@ for r in range(0,sums.shape[0], windowsize_r):
 tms=np.asarray(tms)
 print(np.shape(tms))
 tms=tms.reshape((6,6))
+flat=tms[np.where(tms>0)]
+m2=np.mean(flat)
+s2=np.std(flat)
+maxval2=np.amax(flat)
+minval2=np.amin(flat)
+
+plt.clf()
+plt.cla()
+fig=plt.Figure()
 plt.imshow(tms)
-plt.colorbar(label='Relative Brightness (nLb/TM)')
-plt.show()
+plt.title('Engineering Camera Frame\n Mean='+str('%.1f'%m2)+', $\sigma$='+str('%.1f'%s2)+'\n Max='+str('%.1f'%maxval2)+', Min='+str('%.1f'%minval2))
+plt.colorbar(label='Mean Relative Brightness (nLb/TM)')
+plt.tight_layout()
+plt.savefig('nLb_TM.png')
 print(tms.shape)
