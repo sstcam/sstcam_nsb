15ab233659d8524310045dfd8d485979482fe620
STSpencer <sojourner92@gmail.com>
2021-07-09 17:28:39 +0100

    Updated traybake.py for Hz and added new Eta Car nightmare run

---
+++ results/etacarnightmarev2/README.md
@@ -0,0 +1,13 @@
+Second crack at a 'nightmare' run on Eta Carinae to be more consistent with timescale investigation.
+
+starttime = Time('2022-05-16T07:00:00')                                         
+loc = EarthLocation.from_geodetic(lon=-70.317876,lat=-24.681546,height=2176.6*u\
+.m) #SST1 Paranal Position                                                      
+obsalt = 13.059164827805311*u.deg
+obsaz = 209.55492271574875*u.deg
+raval=161.26479166666664*u.deg
+decval=-59.68443055555555*u.deg
+
+TAN projection
+gaia mag  = 15
+healpix level = 11
--- scripts/traybake.py
+++ scripts/traybake.py
@@ -5,18 +5,23 @@ from astropy.table import Table, Column, MaskedColumn
 from matplotlib import cm
 import matplotlib as mpl
 from scipy.ndimage import rotate
-inputtable=Table.read('/home/spencers/sstcam_nsb/results/etacardarkgauss3/fluxes_crota_0.csv')
+inputtable=Table.read('/home/spencers/sstcam_nsb/results/etacarnightmarev2/fluxes_crota_0.csv')
 
-print(inputtable)
+print(inputtable.keys())
 posx=inputtable['xcenter'].data
 posy=inputtable['ycenter'].data
-sums=inputtable['aperture_sum'].data
+sums=inputtable['aperture_sum_nLb'].data
+sHz=inputtable['aperture_sum_Hz'].data/10**6 #Values in MHz
 windowsize_r = 8
 windowsize_c = 8
 mean=np.mean(sums)
+meanhz=np.mean(sHz)
 sig=np.std(sums)
+sighz=np.std(sHz)
 maxval=np.amax(sums)
 minval=np.amin(sums)
+maxvalhz=np.amax(sHz)
+minvalhz=np.amin(sHz)
 
 def cam_squaremaker(data):
     '''Function to translate CHEC-S integrated images into square arrays for
@@ -48,6 +53,8 @@ fig=plt.Figure()
 plt.imshow(sums,cmap=cm.viridis,norm=mpl.colors.LogNorm())
 plt.title('Engineering Camera Frame\n Mean='+str('%.1f'%mean)+', $\sigma$='+str('%.1f'%sig)+'\n Max='+str('%.1f'%maxval)+', Min='+str('%.1f'%minval))
 plt.colorbar(label='Relative Brightness (nLb/Pixel)')
+plt.xlabel('x (Pixels)')
+plt.ylabel('y (Pixels)')
 plt.tight_layout()
 plt.savefig('nLb_pixel.png')
 
@@ -75,6 +82,51 @@ fig=plt.Figure()
 plt.imshow(tms)
 plt.title('Engineering Camera Frame\n Mean='+str('%.1f'%m2)+', $\sigma$='+str('%.1f'%s2)+'\n Max='+str('%.1f'%maxval2)+', Min='+str('%.1f'%minval2))
 plt.colorbar(label='Mean Relative Brightness (nLb/TM)')
+plt.xlabel('x (TMs)')
+plt.ylabel('y (TMs)')
 plt.tight_layout()
 plt.savefig('nLb_TM.png')
+print(tms.shape)
+
+sHz=np.asarray(cam_squaremaker(sHz))
+sHz=np.rot90(sHz,3)
+plt.clf()
+plt.cla()
+fig=plt.Figure()
+plt.imshow(sHz,cmap=cm.viridis,norm=mpl.colors.LogNorm())
+plt.title('Engineering Camera Frame\n Mean='+str('%.1f'%meanhz)+' MHz, $\sigma$='+str('%.1f'%sighz)+' MHz\n Max='+str('%.1f'%maxvalhz)+' MHz, Min='+str('%.1f'%minvalhz)+' MHz')
+plt.colorbar(label='Relative Brightness (MHz/Pixel)')
+plt.xlabel('x (Pixels)')
+plt.ylabel('y (Pixels)')
+plt.tight_layout()
+plt.savefig('Hz_pixel.png')
+
+tmshz=[]
+
+# Crop out the window and calculate the histogram
+for r in range(0,sHz.shape[0], windowsize_r):
+    for c in range(0,sHz.shape[1], windowsize_c):
+        window = sHz[r:r+windowsize_r,c:c+windowsize_c]
+        print(window)
+        tmshz.append(np.mean(window))
+
+tmshz=np.asarray(tmshz)
+print(np.shape(tmshz))
+tmshz=tmshz.reshape((6,6))
+flathz=tmshz[np.where(tmshz>0)]
+m2hz=np.mean(flathz)
+s2hz=np.std(flathz)
+maxval2hz=np.amax(flathz)
+minval2hz=np.amin(flathz)
+
+plt.clf()
+plt.cla()
+fig=plt.Figure()
+plt.imshow(tmshz)
+plt.title('Engineering Camera Frame\n Mean='+str('%.1f'%m2hz)+' MHz, $\sigma$='+str('%.1f'%s2)+' MHz\n Max='+str('%.1f'%maxval2hz)+' MHz, Min='+str('%.1f'%minval2hz)+' MHz')
+plt.colorbar(label='Mean Relative Brightness (MHz/TM)')
+plt.xlabel('x (TMs)')
+plt.ylabel('y (TMs)')
+plt.tight_layout()
+plt.savefig('Hz_TM.png')
 print(tms.shape)
