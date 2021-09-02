cdadc4b16863d1fffe576ab32c9df21cb4428c2c
STSpencer <sojourner92@gmail.com>
2021-09-02 13:16:31 +0100

    Upgraded starbinner to healpix

--- scripts/starbinner.py
+++ scripts/starbinner.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import astropy.units as u
 from astropy.time import Time
@@ -15,33 +14,111 @@ from skyfield.api import Star, load
 from skyfield.data import hipparcos
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import pandas as pd
+import healpy as hp
+from astropy.coordinates import SkyCoord
+import healpy as hp
+import numpy as np
+import matplotlib.gridspec as gridspec
+
+def cat2hpx(lon, lat, nside, radec=True):
+    """
+    Convert a catalogue to a HEALPix map of number counts per resolution
+    element.
+
+    Parameters
+    ----------
+    lon, lat : (ndarray, ndarray)
+        Coordinates of the sources in degree. If radec=True, assume input is in the icrs
+        coordinate system. Otherwise assume input is glon, glat
+
+    nside : int
+        HEALPix nside of the target map
+
+    radec : bool
+        Switch between R.A./Dec and glon/glat as input coordinate system.
+
+    Return
+    ------
+    hpx_map : ndarray
+        HEALPix map of the catalogue number counts in Galactic coordinates
+
+    """
+
+    npix = hp.nside2npix(nside)
+
+    if radec:
+        eq = SkyCoord(lon, lat, frame='icrs', unit='deg')
+        l, b = eq.galactic.l.value, eq.galactic.b.value
+    else:
+        l, b = lon, lat
+
+    # conver to theta, phi
+    theta = np.radians(90. - b)
+    phi = np.radians(l)
+
+    # convert to HEALPix indices
+    indices = hp.ang2pix(nside, theta, phi)
+
+    idx, counts = np.unique(indices, return_counts=True)
+
+    # fill the fullsky map
+    hpx_map = np.zeros(npix, dtype=int)
+    hpx_map[idx] = counts
+
+    return hpx_map
+
+def pixarea2nside(area):
+    """Closest HEALPix nside for a given area, shamelessly stolen from desitarget.
+    Parameters
+    ----------
+    area : :class:`float`
+        area in square degrees.
+    Returns
+    -------
+    :class:`int`
+        HEALPix nside that corresponds to passed area.
+    Notes
+    -----
+        - Only considers 2**x nside values (1, 2, 4, 8 etc.)
+    """
+    # ADM loop through nsides until we cross the passed area.
+    nside = 1
+    while (hp.nside2pixarea(nside, degrees=True) > area):
+        nside *= 2
+
+    # ADM there is no nside of 0 so bail if nside is still 1.
+    if nside > 1:
+        # ADM is the nside with the area that is smaller or larger
+        # ADM than the passed area "closest" to the passed area?
+        smaller = hp.nside2pixarea(nside, degrees=True)
+        larger = hp.nside2pixarea(nside//2, degrees=True)
+        if larger/area < area/smaller:
+            return nside//2
+
+    return nside
 
 if __name__=='__main__':
+    NSIDE=8
+    print('Appropriate NSIDE to use',pixarea2nside(52.524))
     df=pd.read_csv('/home/spencers/gaiacat8.txt')
     highmaglimit=4
-    lowmaglimit=8
+    lowmaglimit=7
     covlimit=10
 
     df=df[df['Magnitude'] <= lowmaglimit]
     df=df[df['Magnitude'] > highmaglimit]
     ra=df['RA']
     dec=df['DEC']
-    #nbins=(np.linspace(0,360,18),np.linspace(-90,90,36)) #10x10 degree bins
-    nbins=(np.linspace(0,360,25),np.linspace(-90,90,51)) #7x7 degree bins
-    hist,yedges,xedges=np.histogram2d(ra,dec,nbins)
-    mstars=np.mean(hist)
-    lenhist=np.shape(hist)[0]*np.shape(hist)[1]
-    covlocs=hist[hist>covlimit]
-    skycov=(np.float(len(covlocs))/lenhist)*100
-    extent = [0,360,-90,90]
-    fig=plt.figure(figsize=(12,6))
-    ax = plt.gca()
-    plt.xlabel('RA (deg)')
-    plt.ylabel('DEC (deg)')
-    plt.title('Hipparcos Stars with a Magnitude Brighter or Equal to '+str(lowmaglimit)+' mag,\n But Dimmer Than '+str(highmaglimit)+' mag.\n Mean Stars per Bin: '+str(mstars)+'\n Sky Coverage Assuming '+str(covlimit)+' Stars Needed: '+str(skycov)+'%')
-    im=ax.imshow(hist,extent=extent)
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(im,label='Number of Stars Present',cax=cax)
-    plt.tight_layout()
+    hpx_map=cat2hpx(ra,dec,nside=NSIDE,radec=True)
+    print(len(np.where(hpx_map>covlimit)[0]),hp.nside2npix(NSIDE))
+    skycov=len(np.where(hpx_map>covlimit)[0])/float(hp.nside2npix(NSIDE))*100
+    mstars=np.mean(hpx_map)
+    gs = gridspec.GridSpec(2, 1,height_ratios=[1,200])
+    ax1 = plt.subplot(gs[0])
+    ax2 = plt.subplot(gs[1])
+    #plt.axes(ax1)
+    ax1.axis('off')
+    plt.suptitle('Gaia Stars with a Magnitude Brighter or Equal to '+str(lowmaglimit)+' mag,\n but Dimmer Than '+str(highmaglimit)+' mag.\n Bin Size: '+'%.3f'%hp.nside2pixarea(NSIDE)+' Steradians, Mean Stars per Bin: '+str('%.3f'%mstars)+'\n Sky Coverage Assuming '+str(covlimit)+' Stars Needed: '+str('%.3f'%skycov)+'%')
+    plt.axes(ax2)
+    im=hp.mollview(hpx_map,title='',unit='Number of Stars',hold=True,xsize=1200)
     plt.savefig('starhist'+str(highmaglimit)+'_'+str(lowmaglimit)+'_'+str(covlimit)+'_7deg.png',dpi=300)
