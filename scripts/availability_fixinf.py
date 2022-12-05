65517095e5050b2337417ab0bbdbedbfd0467add
Samuel Timothy Spencer <31512502+STSpencer@users.noreply.github.com>
2022-12-05 14:42:19 +0100

    Add files via upload
    Verion of availability.py with fixed infinity line.

---
+++ scripts/availability_fixinf.py
@@ -0,0 +1,85 @@
+import astropy.units as u
+from astropy.time import Time
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+from matplotlib.ticker import MaxNLocator
+from matplotlib.lines import Line2D
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astroquery.vizier import Vizier
+mpl.use('Agg')
+from skyfield.api import Star, load
+from skyfield.data import hipparcos
+
+def calc_field_rotation(Lat,Azs,Alts):
+    K=15.04/3600 #Work in seconds
+    Azs=Azs*np.pi/180.0
+    nonvalid=np.where(np.logical_or(abs(Alts-90.0)<0.2,abs(Alts-270.0)<0.2))
+    print(Alts,nonvalid,np.amax(Alts),np.amin(Alts))
+
+    Alts=Alts*np.pi/180.0
+    Lat=Lat*np.pi/180.0 #Must be in rad for numpy
+    rots=K*np.cos(Lat)*np.cos(Azs)/np.cos(Alts)
+    rots[nonvalid]=np.inf
+    return rots
+
+if __name__=='__main__':
+    print(hipparcos.URL)
+    obstime = Time('2022-03-24T07:32:00.000')
+    loc = EarthLocation.from_geodetic(lon=-70.317876,lat=-24.681546,height=2176.6*u.m) #SST1 Paranal Position                                                                                                                                
+    maglimit=4
+    plotstars=True
+    Maxtrack=0.19/10
+    Lat=loc.lat
+    with load.open('hip_main.dat') as f:
+        df = hipparcos.load_dataframe(f)
+    df=df[df['magnitude']<=maglimit]
+
+    ts=load.timescale()
+    t=ts.from_astropy(obstime)
+    planets = load('de421.bsp')
+    earth = planets['earth']
+    stars= Star.from_dataframe(df)
+    astrometric = earth.at(t).observe(stars)
+    ra, dec, distance = astrometric.radec()
+    sc=SkyCoord(ra=ra.radians*u.rad,dec=dec.radians*u.rad)
+    AltAz=AltAz(location=loc,obstime=obstime)
+    sc=sc.transform_to(AltAz)
+    alts=sc.alt.deg
+    azs=sc.az.deg
+    Altvals=np.linspace(20,89.2,1000)
+    Azvals=np.linspace(0,360,1000)
+    #Azvals=np.concatenate((np.linspace(0,70,250),np.linspace(110.0,250.0,500),np.linspace(290,360,250)))
+    Azs,Alts=np.meshgrid(Altvals,Azvals)
+    print(Azs,Alts)
+    rots=calc_field_rotation(Lat,Azs,Alts)
+    vmin=-Maxtrack
+    vmax=Maxtrack
+    cmap = plt.get_cmap('PiYG')
+    levels = MaxNLocator(nbins=1000).tick_values(vmin, vmax)
+    cf=plt.contourf(Azs,Alts,rots,levels=levels)
+    if plotstars==True:
+        plt.scatter(alts,azs,marker='+',color='orange')
+    print('cf',cf,np.shape(cf))
+
+    cs=plt.contour(cf,levels=[-Maxtrack,Maxtrack],colors='r',linestyles='dashed')
+
+    for i in cs.collections:
+        print(i)
+    plt.colorbar(cf,label='Degrees/Second')
+    colors = ['red']
+    lines = [Line2D([0], [0], color=c, linewidth=3, linestyle='dashed') for c in colors]
+    if plotstars==True:
+        lines.append(Line2D([],[],color='orange',linestyle='None',marker='+'))
+        labels=['Calibration Limit','Bright Star']
+    else:
+        lables=['Calibration Limit']
+    plt.legend(lines,labels,loc='upper right')
+    plt.xlim(20,89.2)
+    plt.ylim(0,360)
+    plt.xlabel('Altitude (Degrees)')
+    plt.ylabel('Azimuth (Degrees)')
+    plt.title('UTC Time '+str(obstime.utc)+'\n Field Rotation With Stars Brighter Than Mag '+str(maglimit)+'\n Calibration Limit '+'%.4f'%Maxtrack+ ' Degrees/Second')
+    plt.tight_layout()
+
+    plt.savefig('rot.png',dpi=300)
